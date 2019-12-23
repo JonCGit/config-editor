fbefbd0495c3ea277d3c3f384dba90cee51a3468
Dhruv Patel <dhruv.patel@accesscfa.com>
2019-12-23 09:31:56 -0500

    featflag section

--- src/FeatFlagWindow.js
+++ src/FeatFlagWindow.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import DataTable from 'react-data-table-component';
 import './index.css';
-import ConfigJson from './config.json';
 import PopUp from './PopUp.js';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 class FeatFlagWindow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   togglePopup() {
@@ -17,17 +21,60 @@ class FeatFlagWindow extends React.Component {
     });
   }
 
+  handleChange(changeEvent) {
+    this.setState({
+      value: changeEvent.target.value
+    });
+  }
+
+  renderSwitch() {
+    const arrayOrString = this.props.selectedConfig.configValue;
+    if (Array.isArray(arrayOrString)) {
+      return this.props.selectedConfig.configValue.map(options => {
+        return (
+          <FormControlLabel
+            key={options}
+            value={options}
+            control={<Radio color="primary" />}
+            label={options}
+          />
+        )
+      });
+    } else {
+      return (
+        <FormControlLabel
+          key={this.props.selectedConfig.configValue}
+          value={this.props.selectedConfig.configValue}
+          control={<Radio color="primary" />}
+          label={this.props.selectedConfig.configValue}
+        />
+      );
+    }
+  }
+
   render() {
+    const valueOptions = (
+      <FormControl component="fieldset">
+        <RadioGroup value={this.state.value} onChange={this.handleChange}>
+          {this.props.selectedConfig.configValue ?
+              this.renderSwitch() : null
+          }
+        </RadioGroup>
+      </FormControl>
+
+    );
     return (
       <div>
         {this.state.showPopup ?
-          <PopUp text='Close Me' closePopup={this.togglePopup.bind(this)}/>
+          <PopUp value={this.state.value} closePopup={this.togglePopup.bind(this)}/>
           : null
         }
         <div className = "selected-container">
         <h2 className="title">Feature Flag</h2>
         <h4 className="inner-title">Values</h4>
-        <div>{this.props.selectedConfig.configValue}</div>
+        <div className="value-container">
+          {valueOptions}
+        </div>
         <div className="button-container">
           <button className="button" onClick={this.togglePopup.bind(this)}>Add</button>
           <button className="button">Edit</button>
--- src/PopUp.js
+++ src/PopUp.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import DataTable from 'react-data-table-component';
 import './index.css';
-import ConfigJson from './config.json';
+import { IoIosClose } from "react-icons/io";
 
 class PopUp extends React.Component {
   render() {
     return (
       <div className='popup'>
         <div className='popup_inner'>
-          <h1>{this.props.text}</h1>
-          <button onClick={this.props.closePopup}>close me</button>
+        <div className="popup-header">Add Product <IoIosClose className="close-icon" onClick={this.props.closePopup} /></div>
         </div>
       </div>
     );
