b298e2f997694ac6d1f1c66b92cb75f201a3148d
Daniell Yancey <daniel.yancey@accesscfa.com>
2019-12-27 16:36:01 -0500

    formatting fixes

--- src/FeatFlagWindow.js
+++ src/FeatFlagWindow.js
@@ -10,7 +10,7 @@ class FeatFlagWindow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        value: ''
+      value: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -23,23 +23,21 @@ class FeatFlagWindow extends React.Component {
 
   handleChange(changeEvent) {
     this.setState({
-      value: changeEvent.target.value
+      value: changeEvent.target.value,
     });
   }
 
   renderSwitch() {
     const arrayOrString = this.props.selectedConfig.configValue;
     if (Array.isArray(arrayOrString)) {
-      return this.props.selectedConfig.configValue.map(options => {
-        return (
-          <FormControlLabel
-            key={options}
-            value={options}
-            control={<Radio color="primary" />}
-            label={options}
-          />
-        )
-      });
+      return this.props.selectedConfig.configValue.map(options =>
+        <FormControlLabel
+          key={options}
+          value={options}
+          control={<Radio color="primary" />}
+          label={options}
+        />
+      );
     } else {
       return (
         <FormControlLabel
--- src/PopUp.js
+++ src/PopUp.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './index.css';
-import { IoIosClose } from "react-icons/io";
+import { IoIosClose } from 'react-icons/io';
 
 class PopUp extends React.Component {
   render() {
     return (
       <div className='popup'>
         <div className='popup_inner'>
-        <div className="popup-header">Add Product <IoIosClose className="close-icon" onClick={this.props.closePopup} /></div>
+        <div className="popup-header">
+          Add Product <IoIosClose className="close-icon" onClick={this.props.closePopup} />
+        </div>
         </div>
       </div>
     );
